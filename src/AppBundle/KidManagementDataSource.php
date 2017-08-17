4775033e24347e315d439d873cdf8b32377b3e79
Yehia Abdul Hamid <yehiamcp@gmail.com>
2017-08-17 19:54:18 +0200

    kid management data source

---
+++ src/AppBundle/KidManagementDataSource.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace AppBundle;
+use Doctrine\ORM\EntityManager;
+
+/**
+ * Class KidManagementDataSource
+ * @package AppBundle
+ */
+class KidManagementDataSource
+{
+    private static $em ;
+    /**
+     * KidManagementDataSource constructor.
+     * @param EntityManager $em
+     */
+    public function __construct(EntityManager $em)
+    {
+        self::$em = $em;
+    }
+
+
+    /**
+     * @param string $deliveryPersonMobile
+     * @return array
+     */
+    public static function getKidsByDeliveryPersonMobile($deliveryPersonMobile)
+    {
+        if(strlen($deliveryPersonMobile) < 11) return [];
+
+        $conn = self::$em->getConnection();
+        $sql = "select distinct lower(BTRIM(kidname)) as name from kid_management.arrival_order_lines where arrivalorder_id in (
+                        select id from kid_management.arrival_orders where trim(deliverypersonmobile) like trim(:mobile) 
+                ) order by name" ;
+        $stmt = $conn->prepare($sql);
+        $stmt->bindValue("mobile", '%' .$deliveryPersonMobile . '%');
+        $stmt->execute();
+
+        return $stmt->fetchAll();
+    }
+}
