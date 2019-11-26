128b14569ab80c3fd61e002d2449d07ba327958b
Yehia AbdulHamid <yehiamcp@gmail.com>
2019-11-26 03:11:02 +0200

    created new item entity, repository and controller

---
+++ src/AppBundle/Controller/ItemsController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class ItemsController extends Controller
+{
+    /**
+     * @Route("/items")
+     */
+    public function indexAction()
+    {
+        $itemsRepository = $this->getDoctrine()
+                                ->getManager()
+                                ->getRepository(\AppBundle\Entity\ItemsController::class);
+
+        $items = $itemsRepository->findAll();
+        return $this->json(['items'=> $items]);
+    }
+}
---
+++ src/AppBundle/Entity/Items.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Doctrine\Common\Collections\ArrayCollection;
+
+/**
+ * Item
+ *
+ * @ORM\Table(name="items")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\ItemRepository")
+ */
+class Item implements \JsonSerializable
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="auto")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="NAME", type="string")
+     */
+    private $name;
+
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="price", type="integer", nullable=true)
+     */
+    private $price;
+
+
+    public function __construct()
+    {
+    }
+
+    /**
+     * Get id
+     *
+     * @return string
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+
+    /**
+     * Get price
+     *
+     * @return integer
+     */
+    public function getPrice()
+    {
+        return $this->price;
+    }
+
+
+    public function jsonSerialize()
+    {
+        return array(
+            'id' => $this->id,
+            'name'=> $this->name,
+            'price'=> $this->price,
+        );
+    }
+}
+
